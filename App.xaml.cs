75662debbf7ca75a48ce49257dcf6add732a8649
KennonNichols <kenlenichols@gmail.com>
2026-07-27 18:51:46 -0600

    -Now pops up crash log.

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace Filterizer2;
 
@@ -9,4 +10,28 @@ namespace Filterizer2;
 /// </summary>
 public partial class App : Application
 {
+	public App()
+	{
+		DispatcherUnhandledException += App_DispatcherUnhandledException;
+		TaskScheduler.UnobservedTaskException += TaskScheduler_UnobservedTaskException;
+		AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+	}
+	
+	private void App_DispatcherUnhandledException(object sender,
+		DispatcherUnhandledExceptionEventArgs e)
+	{
+		MessageBox.Show(e.Exception.ToString(), "Unhandled Exception");
+	}
+	
+	private void TaskScheduler_UnobservedTaskException(object? sender,
+		UnobservedTaskExceptionEventArgs e)
+	{
+		MessageBox.Show(e.Exception.ToString(), "Unobserved Task Exception");
+	}
+	
+	private void CurrentDomain_UnhandledException(object sender,
+		UnhandledExceptionEventArgs e)
+	{
+		MessageBox.Show(e.ExceptionObject.ToString(), "Unhandled Exception In Domain");
+	}
 }
